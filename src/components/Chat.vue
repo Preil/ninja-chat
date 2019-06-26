8eb1ced9287b374aad6c48a4fb1e80e3ebafaa19
preil <preil2000@gmail.com>
2019-06-26 15:56:43 +0300

    Passing name to the chat component, as a parameter.

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="chat container">
     <h2>Chat</h2>
+    <h3>Hello {{this.name}}</h3>
   </div>
 </template>
 
 <script>
   export default {
     name: 'Chat',
+    props: ['name'],
     data() {
       return {}
     }
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -15,35 +15,38 @@
 </template>
 
 <script>
-export default {
-  name: 'Welcome',
-  data () {
-    return {
-      name: null,
-      feedback: null
-    }
-  },
-  methods: {
-    enterChat() {
-      if(this.name){
+  export default {
+    name: 'Welcome',
+    data() {
+      return {
+        name: null,
+        feedback: null
+      }
+    },
+    methods: {
+      enterChat() {
+        if (this.name) {
+          this.$router.push({name: 'Chat', params: {name: this.name}})
 
-      } else {
-        this.feedback = 'You have to enter a name to join'
+        } else {
+          this.feedback = 'You have to enter a name to join'
+        }
       }
     }
   }
-}
 </script>
 
 <style scoped>
-.welcome{
-  max-width: 400px;
-  margin-top: 100px;
-}
+  .welcome {
+    max-width: 400px;
+    margin-top: 100px;
+  }
+
   .welcome h2 {
     font-size: 3em;
 
   }
+
   .welcome button {
     margin: 30px auto;
   }
